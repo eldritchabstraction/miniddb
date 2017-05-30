006cf4fcc5f4beb648d3478987551f7a053d77d6
eldr <eldr@gmail.com>
2017-05-30 12:43:58 -0700

    Add server.cpp
    - This server can take on multiple simultaneous connections
    - Missing:
        - a couple of handlers for kill connection and kill server
        - collapse function for poll fds
    - Issues:
        - doesn't handle close connection properly
        - everything is a mess atm

--- src/server.h
+++ src/server.h
@@ -1,12 +1,20 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <stdint.h>
+
+#if defined (__cplusplus)
+extern "C" {
+#endif
 
 /*
  * The server independently listens for new connections and passes messages to
  * be processed by main thread
  */
-void server_start(uint16_t bind_port);
+void server_start(int bind_port);
+
+
+#if defined (__cplusplus)
+}
+#endif
 
 #endif // SERVER_H
